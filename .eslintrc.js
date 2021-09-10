fa2c119a42b9ecfb8d2e1c2fd65ddcfe979d5c94
Diego Weinmann <diegoweinmann@gmail.com>
2021-09-10 01:34:49 -0300

    chore: add eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
     'next',
     'next/core-web-vitals',
   ],
